b2d21351a9f685f19e000f73957052142204de42
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-08-03 15:25:28 +0200

    Feature: Add key switching (usage)

--- agent_smith/llm/client.py
+++ agent_smith/llm/client.py
@@ -4,6 +4,7 @@ from openai import (
     InternalServerError,
     OpenAI,
     RateLimitError,
+    PermissionDeniedError,
 )
 from openai.types.chat import ChatCompletionMessageParam
 import time
@@ -17,16 +18,22 @@ class Client:
         api_url: str,
         model_name: str,
         max_tokens: int,
-        api_key: str,
+        api_keys: list[str],
         stop: str = "<end_code>",
     ):
         self.api_url = api_url
         self.model_name = model_name
         self.max_tokens = max_tokens
         self.stop = stop
-        self.client = OpenAI(base_url=api_url, api_key=api_key)
+        self.current_api_key_index = 0
+        self.client = OpenAI(
+            base_url=api_url, api_key=api_keys[self.current_api_key_index]
+        )
+        self.api_keys = api_keys
 
-    def generate(self, conversation: list[ChatCompletionMessageParam]) -> LLMResponse:
+    def generate(
+        self, conversation: list[ChatCompletionMessageParam]
+    ) -> LLMResponse:
         max_retry = 5
         cooldown = 0
         retries = 0
@@ -54,13 +61,35 @@ class Client:
                     llm_output=content,
                 )
             except (
-                RateLimitError,
                 APITimeoutError,
                 APIConnectionError,
                 InternalServerError,
             ) as e:
+                print(f"API error: {e}. Retrying in {cooldown} seconds...")
                 cooldown = min(2**retries, 30)
                 retries += 1
-                if retries >= max_retry + 1:  # la premiere attemps + les retries
+                if (
+                    retries >= max_retry + 1
+                ):  # la premiere attemps + les retries
                     raise e
                 time.sleep(cooldown)
+            except (PermissionDeniedError, RateLimitError) as e:
+                print(
+                    f"Permission denied: {e}. Check your API key and permissions."
+                )
+                if (
+                    self.current_api_key_index + 1 < len(self.api_keys)
+                    and retries < max_retry + 1
+                ):
+                    cooldown = min(2**retries, 30)
+                    print(f"Switching to the next API key in {cooldown}...")
+                    self.current_api_key_index += 1
+                    print("Before:", self.client.api_key)
+                    self.client.api_key = self.api_keys[
+                        self.current_api_key_index
+                    ]
+                    print("After:", self.client.api_key)
+                    retries += 1
+                    time.sleep(cooldown)
+                else:
+                    raise e
